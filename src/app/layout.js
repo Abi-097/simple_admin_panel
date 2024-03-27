9fdac60e0bdef3dbb4af83d248fd8aa871b0d3ef
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-03-27 23:50:17 +0530

    sidebar created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GlobalState from "../context";
+import Header from "@/components/Header/Index";
+import Sidebar from "@/components/Sidebar/Index";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalState>
+          <div className="flex h-screen overflow-hidden">
+            <Sidebar />
+            <div className="relative flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+              <Header />
+              <main>
+                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                  {children}
+                </div>
+              </main>
+            </div>
+          </div>
+        </GlobalState>
+      </body>
     </html>
   );
 }
